4cc0895899500d90bb8373267b6dc6165d0db1a1
gowthamiv <gowthami.525@gmail.com>
2026-05-23 01:57:15 +0530

    Making the restaurant card dynamic

--- App.js
+++ App.js
@@ -52,31 +52,61 @@ const Header = () => {
     );
 }
 
+/**
+ * Sample public data from swiggy/zomato sites 
+ */
+const resObj = {
+                                 "id":"1003414",
+                                 "name":"Pizza Hut",
+                                 "cloudinaryImageId":"RX_THUMBNAIL/IMAGES/VENDOR/2026/2/12/e109ae70-80ca-4f7d-b2a6-2d60706a72b9_1003414.JPG",
+                                 "locality":"Chhindwara",
+                                 "areaName":"Chhindwara City",
+                                 "costForTwo":"₹350 for two",
+                                 "cuisines":[
+                                    "Pizzas"
+                                 ],
+                                 "avgRating":4.3,
+                                 "parentId":"721",
+                                 "avgRatingString":"4.3",
+                                 "totalRatingsString":"273",
+                                 "sla":{
+                                    "deliveryTime":50,
+                                    "lastMileTravel":14.1,
+                                    "serviceability":"SERVICEABLE_WITH_BANNER",
+                                    "slaString":"45-50 mins",
+                                    "lastMileTravelString":"14.1 km",
+                                    "iconType":"ICON_TYPE_EMPTY"
+                                 },
+                                 "availability":{
+                                    "nextOpenTimeMessage":"Opens next at 11 am, today"
+                                 },
+                                 "badges":{
+                                    
+                                 }
+                                };
+
 const Body = () => {
     return (
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
+        <RestaurantCard resData={resObj}/>
       </div>
     </div>
     );
 }
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+    const { resData } = props;
     return (
         <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
-            <img className="res-logo" alt="res-logo" src="https://b.zmtcdn.com/data/pictures/4/19982534/169529ec69e973aa53d15c3d63535347_featured_v2.jpg?output-format=webp">
+            <img className="res-logo" alt="res-logo" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660,e_grayscale/"+resData.cloudinaryImageId}>
             </img>
-            <h3>Sylas Kitchen</h3>
-            <h3>Biryani, North, Asian</h3>
-            <h3>4.6</h3>
-            <h3>20 minutes</h3>
+            <h3>{resData.name}</h3>
+            <h3>{resData.cuisines.join(", ")}</h3>
+            <h3>{resData.avgRating} stars</h3>
+            <h3>{resData.costForTwo}</h3>
+            <h3>{resData.sla.deliveryTime} minutes</h3>
         </div>
     );
 }
